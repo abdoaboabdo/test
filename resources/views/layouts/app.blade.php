9ca7775765daf47c10d6c1cd722145c0d6a119af
Rant <Rant@DESKTOP-DHP2JI6>
2019-06-19 16:38:20 +0300

    make it better

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
           <link href="{{asset('css/nav.css')}}" rel="stylesheet" type="text/css">
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
         <!-- Styles -->
